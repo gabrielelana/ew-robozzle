d1e152811c1c8d2e7a5f9e757d7656ff226e96f4
gabriele.lana <gabriele.lana@cleancode.it>
2015-11-17 10:04:43 +0100

    Acceptance tests using assert_scenario

---
+++ acceptance_tests.exs
@@ -0,0 +1,44 @@
+defmodule Robozzle.AcceptanceTest do
+  use ExUnit.Case, async: true
+  import Robozzle
+
+  test "incomplete" do
+    assert_scenario :incomplete, "beb.b*", f1: []
+    assert_scenario :incomplete, "beb.b*", f1: [:forward]
+  end
+
+  test "complete" do
+    assert_scenario :complete, "beb.b*", f1: [:forward, :forward]
+    assert_scenario :complete, """
+                               b.b.b.bsb.b.b.
+                               b.b.b.b.b.b.b.
+                               b.b.b.b.b.b*b.
+                               """,
+                               f1: [:forward, :forward, :left, :forward, :forward]
+  end
+
+  test "out of stage" do
+    assert_scenario :out_of_stage, "bnb*", f1: [:forward]
+  end
+
+  test "out of time" do
+    assert_scenario :out_of_time, "bnb*", f1: [:right, {:call, :f1}]
+  end
+
+  test "stack overflow" do
+    assert_scenario :stack_overflow, "bnb*", f1: [:right, {:call, :f1}, :left]
+  end
+
+  test "stairs scenario" do
+    assert_scenario :complete,
+                    """
+                    ..........b*b*
+                    ........b*b*..
+                    ......b*b*....
+                    ....b*b*......
+                    ..b*b*........
+                    beb*..........
+                    """,
+                    f1: [:forward, :left, :forward, :right, {:call, :f1}]
+  end
+end

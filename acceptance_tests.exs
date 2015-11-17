7e3d26db8b60c71cefe142f77192258cb30c0d4f
gabriele.lana <gabriele.lana@cleancode.it>
2015-11-17 11:07:24 +0100

    Implement scenario macro

--- acceptance_tests.exs
+++ acceptance_tests.exs
@@ -1,5 +1,44 @@
+defmodule Robozzle.AcceptanceTest.Macro do
+  defmacro __using__(_opts) do
+    quote do
+      import Robozzle.AcceptanceTest.Macro
+      import Robozzle
+
+      Module.put_attribute __MODULE__, :acceptance_counter, 0
+    end
+  end
+
+  defmacro scenario(outcome, block) do
+    quote do
+      Module.put_attribute(__MODULE__, :acceptance_counter,
+        Module.get_attribute(__MODULE__, :acceptance_counter) + 1)
+      scenario(unquote(outcome),
+               "##{Module.get_attribute(__MODULE__, :acceptance_counter)}",
+               unquote(block))
+    end
+  end
+
+  defmacro scenario(outcome, title, do: {:__block__, _, block}) do
+    quote do
+      test "acceptance #{unquote(title)}" do
+        {ship, stage} = parse(unquote(scenario_from block))
+        fs = %{f1: unquote(function_from :f1, block),
+               f2: unquote(function_from :f2, block),
+               f3: unquote(function_from :f3, block)}
+        outcome = unquote(outcome)
+        assert {^outcome, _, _} = run(fs, ship, stage)
+      end
+    end
+  end
+
+  defp scenario_from(block), do: Enum.find(block, &is_binary/1)
+  defp function_from(f, block), do: Enum.find(block, &match?({^f, _, _}, &1))
+                                    |> (fn({_, _, [cs]}) -> cs; (nil) -> [] end).()
+end
+
 defmodule Robozzle.AcceptanceTest do
   use ExUnit.Case
+  use Robozzle.AcceptanceTest.Macro
 
   scenario(:incomplete) do
     """
@@ -52,7 +91,7 @@ defmodule Robozzle.AcceptanceTest do
     f1 [:right, {:call, :f1}]
   end
 
-  scenario(:complete) do
+  scenario(:complete, "stairs") do
     """
     ..........b*b*
     ........b*b*..

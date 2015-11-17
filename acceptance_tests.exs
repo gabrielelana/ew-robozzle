9dc1859f8859cb75cfe93a0496bd987bbe19260b
gabriele.lana <gabriele.lana@cleancode.it>
2015-11-17 10:21:44 +0100

    Acceptance tests using scenario macro

--- acceptance_tests.exs
+++ acceptance_tests.exs
@@ -1,52 +1,66 @@
 defmodule Robozzle.AcceptanceTest do
-  use ExUnit.Case, async: true
-  import Robozzle
+  use ExUnit.Case
 
-  test "incomplete" do
-    assert_scenario :incomplete, "beb.b*", f1: []
-    assert_scenario :incomplete, "beb.b*", f1: [:forward]
+  scenario(:incomplete) do
+    """
+    beb.b*
+    """
+    f1 []
   end
 
-  test "complete" do
-    assert_scenario :complete, "beb.b*", f1: [:forward, :forward]
-    assert_scenario :complete, """
-                               b.b.b.bsb.b.b.
-                               b.b.b.b.b.b.b.
-                               b.b.b.b.b.b*b.
-                               """,
-                               f1: [:forward, :forward, :left, :forward, :forward]
+  scenario(:incomplete) do
+    """
+    beb.b*
+    """
+    f1 [:forward]
   end
 
-  test "out of stage" do
-    assert_scenario :out_of_stage, "bnb*", f1: [:forward]
+  scenario(:complete) do
+    """
+    beb.b*
+    """
+    f1 [:forward, :forward]
   end
 
-  test "out of time" do
-    assert_scenario :out_of_time, "bnb*", f1: [:right, {:call, :f1}]
+  scenario(:complete) do
+    """
+    b.b.b.bsb.b.b.
+    b.b.b.b.b.b.b.
+    b.b.b.b.b.b*b.
+    """
+    f1 [:forward, :forward, :left, :forward, :forward]
   end
 
-  test "stack overflow" do
-    assert_scenario :stack_overflow, "bnb*", f1: [:right, {:call, :f1}, :left]
+  scenario(:out_of_stage) do
+    """
+    bnb*
+    """
+    f1 [:forward]
   end
 
-  test "stairs scenario" do
-    assert_scenario :complete,
-                    """
-                    ..........b*b*
-                    ........b*b*..
-                    ......b*b*....
-                    ....b*b*......
-                    ..b*b*........
-                    beb*..........
-                    """,
-                    f1: [:forward, :left, :forward, :right, {:call, :f1}]
+  scenario(:stack_overflow) do
+    """
+    bnb*
+    """
+    f1 [:right, {:call, :f1}, :left]
   end
 
-  defp assert_scenario(outcome, scenario, args) do
-    {ship, stage} = parse(scenario)
-    fs = %{f1: Keyword.get(args, :f1, []),
-           f2: Keyword.get(args, :f2, []),
-           f3: Keyword.get(args, :f3, [])}
-    assert {^outcome, _, _} = run(fs, ship, stage)
+  scenario(:out_of_time) do
+    """
+    bnb*
+    """
+    f1 [:right, {:call, :f1}]
+  end
+
+  scenario(:complete) do
+    """
+    ..........b*b*
+    ........b*b*..
+    ......b*b*....
+    ....b*b*......
+    ..b*b*........
+    beb*..........
+    """
+    f1 [:forward, :left, :forward, :right, {:call, :f1}]
   end
 end

d5dcc2e54e5f06fbe69c39166b2531fc531db021
gabriele.lana <gabriele.lana@cleancode.it>
2015-11-17 15:38:50 +0100

    Tests for interactive server

--- test/robozzle_runner_server_test.exs
+++ test/robozzle_runner_server_test.exs
@@ -15,4 +15,36 @@ defmodule Robozzle.Runner.ServerTest do
 
     assert {:complete, _, _} = Runner.Server.run(runner, functions, ship, stage)
   end
+
+  test "interactive", %{runner: runner} do
+    {ship, stage} = parse("beb.b.b*")
+    functions = %{f1: [:forward, :forward, :forward]}
+
+    assert :ok == Runner.Server.load(runner, functions, ship, stage)
+    assert {_ship, _stage} = Runner.Server.step(runner)
+    assert {_ship, _stage} = Runner.Server.step(runner)
+    assert {:complete, _, _} = Runner.Server.step(runner)
+  end
+
+  test "interactive reset at begin", %{runner: runner} do
+    {ship, stage} = parse("beb.b.b*")
+    functions = %{f1: [:forward, :forward, :forward]}
+
+    assert :ok == Runner.Server.load(runner, functions, ship, stage)
+    assert {_ship, _stage} = Runner.Server.step(runner)
+    assert :ok == Runner.Server.load(runner, functions, ship, stage)
+    assert {_ship, _stage} = Runner.Server.step(runner)
+    assert {_ship, _stage} = Runner.Server.step(runner)
+    assert {:complete, _, _} = Runner.Server.step(runner)
+  end
+
+  test "interactive reset at run", %{runner: runner} do
+    {ship, stage} = parse("beb.b.b*")
+    functions = %{f1: [:forward, :forward, :forward]}
+
+    assert :ok == Runner.Server.load(runner, functions, ship, stage)
+    assert {_ship, _stage} = Runner.Server.step(runner)
+    assert {:complete, _, _} = Runner.Server.run(runner, functions, ship, stage)
+    assert {:error, :no_scenario} = Runner.Server.step(runner)
+  end
 end
